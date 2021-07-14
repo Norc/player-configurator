fd78eabd09d12b281590937b8f84a54f23bc7dc2
Dan Orcutt <danorc451@gmail.com>
2021-07-14 08:53:33 -0400

    config and actor assign messaging improvements

--- scripts/configurator.js
+++ scripts/configurator.js
@@ -19,13 +19,20 @@ export async function assignActors() {
 //If a player owns exactly one actor, assign that actor to the user.
     console.log("Player Configurator | Assigning actors");
     for (let u in game.users.contents) {
-        if (!u.isGM ) {
+        if ( !u.isGM && u.data.character ) {
             let player = game.users.contents[u];
             let ownedActors = game.actors.filter( (actor) => actor.data.permission[player.id] == 3);
-            console.log
+            console.log("Player Configurator | assigning actor")
             console.log(ownedActors)
             let obj = {};
-            if (ownedActors && ownedActors.length == 1) obj['character'] = ownedActors[0].id;
+            if ( ownedActors && ownedActors.length === 1 ) {
+                obj['character'] = ownedActors[0].id;
+            } else {
+                if ( ownedActors.length > 1 ) {
+                    ui.notifications.console.warn(`Please ensure that only one actor is assigned to ${player.name}`);
+                }
+            }
+            
             console.log("Player Configurator | Assigning an actor.");
             //update the player with the new properties
             await player.update(obj);
--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ import { configHotbars } from "./configurator.js"
 import { assignActors } from "./configurator.js"
 
 Hooks.once('init', async function() {
-
+    console.log("Player Configurator | Init");
 });
 
 Hooks.once('ready', async function() {
