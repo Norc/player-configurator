83d806f03a2c2eef07c9e4046ddb10d56175b3cd
Dan Orcutt <danorc451@gmail.com>
2021-07-15 11:53:21 -0400

    added to-do notes

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,13 @@ Hooks.once('init', async function() {
 Hooks.once('ready', async function() {
     //test ID array: ["GuMMVgoN63m6AgQj"]
     if(game.users.current.isGM) {
+        /*  TO DO:
+            1. Add right-click listener to Players row of PlayerList to Bulk Config Players and Bulk Config Macros
+            2. Add right-click listener to own username row to "Load" or "Set" player's macro hotbar.
+            3. As GM, add context menu option to own username row to "Stash" or "Restore" your own macro hotbar
+            4. Add Custom Hotbar support?
+        */
+
         await configHotbars();
         await assignActors();
     }
