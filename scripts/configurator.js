8d9dace1377693dcece9d1172109b28c6e438936
Dan Orcutt <danorc451@gmail.com>
2021-07-14 23:54:19 -0400

    improved code and user messaging of assignActors

--- scripts/configurator.js
+++ scripts/configurator.js
@@ -18,27 +18,40 @@ export async function configHotbars(uList = game.users.contents) {
 export async function assignActors() {
 //If a player owns exactly one actor, assign that actor to the user.
     console.log("Player Configurator | Assigning actors");
+    let newAssign = false;
     for (let u in game.users.contents) {
         console.log(typeof (game.users.contents[u]));
-        if ( typeof (game.users.contents[u]) == "object" ) {
+        if ( typeof (game.users.contents[u]) === "object" ) {
             console.log("play");
             let player = game.users.contents[u];
-            if ( ( !u.isGM ) && ( player.data.character === null ) ) {
+            if ( ( !player.isGM ) && ( player.data.character === null ) ) {
                 let ownedActors = game.actors.filter( (actor) => actor.data.permission[player.id] == 3);
                 let obj = {};
-                if ( ownedActors && ownedActors.length === 1 ) {
-                    obj['character'] = ownedActors[0].id;
-                } else {
-                    if ( ownedActors.length > 1 ) {
-                        ui.notifications.warn(`Please ensure that only one actor is assigned to ${player.name}`);
-                    }
+                switch (ownedActors.length) {
+                    case 0:
+                        ui.notifications.warn(`${player.name} does not own an actor. Please assign one manually.`);
+                    break;
+                    case 1:
+                        console.log(`Player Configurator | Assigning ${player} an actor.`);
+                        newAssign = true;
+                        obj['character'] = ownedActors[0].id;
+                        await player.update(obj);
+                    break;
+                    default:
+                        ui.notifications.warn(`More than one actor is owned by ${player.name}. Please pick one manually.`);
                 }
-                
-                //update the player with the new properties
-                console.log(`Player Configurator | Assigning ${player} an actor.`);
-                await player.update(obj);
-                console.log(player.data.character);
             }
+        }
+    }
+
+    //display success message if all players have assigned actors
+    let users = game.users.contents.filter( (u) => typeof u === "object");
+    let unassignedPlayers = users.filter( (u) => ( (u.data.character === null) && ( !u.isGM ) ) );
+    if ( unassignedPlayers.length == 0  ) {
+        if (newAssign) {
+            ui.notifications.info("Successfully assigned characters. All players now have an assigned character.");
+        } else {
+            ui.notifications.info("All players already had an assigned character. No changes made.");
         }
     }
 }
